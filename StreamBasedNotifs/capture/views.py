17a0ef227460a14c80ca8534d36e7b582e3793ea
Kagan <bkaganyildiz@github.com>
2017-04-26 23:02:48 +0300

    First controller layout

--- StreamBasedNotifs/capture/views.py
+++ StreamBasedNotifs/capture/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_protect , requires_csrf_token , csrf_exempt #for csrf protection
 # Create your views here.
+
+@csrf_exempt
+def captureEvents(request) :
+    '''
+    Call Listener class that returns json object parse it and
+    send it to a template
+    '''
+    form = "hello"
+    return render(request,"capture.html",{'form' : form , 'title' : 'Remove Key'})
