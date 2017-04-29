52dbf2e31b8fa7769bc2465ee3da507330d7bb37
Kagan <bkaganyildiz@github.com>
2017-04-29 19:18:49 +0300

    Notification model has been added

--- StreamBasedNotifs/capture/models.py
+++ StreamBasedNotifs/capture/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 # Create your models here.
 
-class Stream(models.Model) :
+
+class Stream(models.Model):
+    '''for saving new streams'''
     name = models.CharField(max_length=255)
-    user_type = models.CharField(max_length=255)
-    user_id = models.CharField(max_length=255)
-    associated_user_ids = models.TextField() #List of string array stored as a comma separated text
+    user_type = models.CharField(max_length=255, blank=True)
+    user_id = models.CharField(max_length=255, blank=True)
+    associated_user_ids = models.TextField(blank=True) #List of string array stored as a comma separated text
     timestamp = models.IntegerField(default=0)
     info = models.TextField() # List of tuples eg.: name : <string> stored as comma separated  text
     def __unicode__(self):
         return self.name
+
+
+class Notification(models.Model):
+    '''saving notifications'''
+    name = models.CharField(max_length=255)
+    event_name = models.CharField(max_length=255)
+    target = models.BooleanField(default=False) #if false user , else assoc. user
+    delay = models.IntegerField(default=0)
+    url =  models.textfield()
+    def __unicode__(self):
+        return  self.name
