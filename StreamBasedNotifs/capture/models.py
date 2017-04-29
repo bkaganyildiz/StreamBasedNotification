cfe2be198fba95eb8dae4d9daaae99806cea894c
Kagan <bkaganyildiz@github.com>
2017-04-29 15:59:21 +0300

    Stream model has been added

--- StreamBasedNotifs/capture/models.py
+++ StreamBasedNotifs/capture/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
 # Create your models here.
+
+class Stream(models.Model) :
+    name = models.CharField(max_length=255)
+    user_type = models.CharField(max_length=255)
+    user_id = models.CharField(max_length=255)
+    associated_user_ids = models.TextField() #List of string array stored as a comma separated text
+    timestamp = models.IntegerField(default=0)
+    info = models.TextField() # List of tuples eg.: name : <string> stored as comma separated  text
+    def __unicode__(self):
+        return self.name
