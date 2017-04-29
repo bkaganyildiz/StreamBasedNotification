01b90901b66addc6eae74048efd4fdcbf2f8611c
Kagan <bkaganyildiz@github.com>
2017-04-29 19:21:47 +0300

    Label text change

--- StreamBasedNotifs/capture/forms.py
+++ StreamBasedNotifs/capture/forms.py
@@ -3,13 +3,13 @@ from .models import Stream
 
 
 class NotificationForm(forms.Form):
-    name = forms.CharField(required=True, label="Notification Name : ")
+    name = forms.CharField(required=True, label="Notification Name ")
     event = forms.ChoiceField(widget=forms.Select(attrs={'class': 'select-style'}), required=True,
-                              label="Event Name : ")
+                              label="Event Name ")
     target = forms.ChoiceField(required=True, choices=[(0, "User"), (1, "Associated User")],
-                               widget=forms.Select(attrs={'class': 'select-style'}), label="Target : ")
-    delay = forms.IntegerField(required=True, label="Delay : ")
-    url = forms.URLField(label="Webhook URL : ", required=True)
+                               widget=forms.Select(attrs={'class': 'select-style'}), label="Target ")
+    delay = forms.IntegerField(required=True, label="Delay ")
+    url = forms.URLField(label="Webhook URL ", required=True)
 
     def __init__(self, *args, **kwargs):
         '''In order to update dropdown for new coming events '''
